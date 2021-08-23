e1fc5ba9d997f1ddb62d4eec853f20e9e7b69801
wray <william.ray@sinergise.com>
2021-08-23 10:34:52 +0200

    constants script added

---
+++ src/stactools/constants.py
@@ -0,0 +1,17 @@
+
+import pystac
+from pystac.link import Link
+
+from pystac.extensions.eo import Band
+
+INSPIRE_METADATA_ASSET_KEY = "inspire-metadata"
+SAFE_MANIFEST_ASSET_KEY = "safe-manifest"
+PRODUCT_METADATA_ASSET_KEY = "product-metadata"
+
+SENTINEL_LICENSE = Link(
+    rel="license",
+    target="https://sentinel.esa.int/documents/"
+    + "247904/690755/Sentinel_Data_Legal_Notice",
+)
+
+SENTINEL_CONSTELLATION = "Sentinel 3"
